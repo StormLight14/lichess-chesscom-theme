1adbdcf4b9568a3ca8c8a0b6373afa3dead797c9
storm <stormpower44@protonmail.com>
2023-09-04 22:29:22 -0600

    Update lichess-to-chesscom.js

--- lichess-to-chesscom.js
+++ lichess-to-chesscom.js
@@ -1,1 +1,22 @@
-document.body.style.border = "5px solid red";
+document.body.style.border = "4px solid purple";
+document.body.style.background = "#312e2b";
+document.querySelectorAll('a').forEach(function (element) {
+  element.style.color = "#FFFFFF";
+  console.log(element.getAttribute('href'));
+
+  if (element.getAttribute('href') === '/blog/community' || element.getAttribute('href') === '/watch') {
+    element.remove();
+  }
+
+});
+let user_tag = document.getElementById('user_tag');
+user_tag.textContent = "⚙";
+
+document.querySelectorAll('span').forEach(function (element) {
+  element.style.color = "#FFFFFF";
+});
+
+document.body.innerHTML = document.body.innerHTML
+  .replace('lichess', 'Chess')
+  .replace('.org', '.com')
+  .replace('Donate', '');
